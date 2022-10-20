87a197a26087ebdb6b4b0ec7731e9e93069cdc2b
liasantos <liasilva77@acad.charqueadas.ifsul.edu.br>
2022-10-20 18:03:06 -0300

    feat: add logo function

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ const creditCardBGColor01 = document.querySelector(
 const creditCardBGColor02 = document.querySelector(
   ".cc-bg svg > g g:nth-child(2) path"
 )
+const creditCardLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
 function setCardType(type) {
   const colors = {
@@ -15,6 +16,9 @@ function setCardType(type) {
   }
   creditCardBGColor01.setAttribute("fill", colors[type][0])
   creditCardBGColor02.setAttribute("fill", colors[type][1])
+  creditCardLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
 setCardType("mastercard")
+
+globalThis.setCardType = setCardType
